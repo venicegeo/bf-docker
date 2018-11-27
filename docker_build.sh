d97ea38046ddcdf7553753145c9b280728e373e0
Patrick D <patrick.doody@rgi-corp.com>
2018-11-27 08:33:42 -0500

    Enable Maven Profile

--- docker_build.sh
+++ docker_build.sh
@@ -3,5 +3,5 @@
 # Building all of the required Beachfront applications
 git clone https://github.com/venicegeo/pz-jobcommon.git
 pushd pz-jobcommon
-mvn clean install
+mvn -Pbeachfront clean install
 popd
