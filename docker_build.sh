9f506f5183ba7a2fa3e77724e04e0f1dc25c4491
Patrick D <patrick.doody@rgi-corp.com>
2018-11-27 10:05:34 -0500

    Building all Java

--- docker_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Building all of the required Beachfront applications
-git clone https://github.com/venicegeo/pz-jobcommon.git
-pushd pz-jobcommon
-mvn -Pbeachfront clean install
-popd
---
+++ docker_javabuild.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Building all of the Beachfront Java applications
+
+# Common libraries first
+git clone https://github.com/venicegeo/pz-jobcommon.git
+pushd pz-jobcommon
+mvn -Pbeachfront clean install -DskipTests
+popd
+
+# Build all remaining Java projects
+declare -a projects=("pz-access"
+                     "pz-gateway"
+                     "pz-idam"
+                     "pz-ingest"
+                     "pz-jobmanager"
+                     "pz-servicecontroller"
+                     "bf-api")
+for project in "${projects[@]}"
+do
+  git clone https://github.com/venicegeo/$project
+  pushd $project
+  mvn org.codehaus.mojo:versions-maven-plugin:2.7:use-dep-version -Dincludes=org.venice.piazza:pz-jobcommon -DdepVersion=LATEST -DforceVersion=true
+  mvn -Pbeachfront clean install -DskipTests
+  popd
+done
