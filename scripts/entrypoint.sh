f94ec998b6bc778a86887c1c446afe9d903a9497
Patrick D <patrick.doody@rgi-corp.com>
2018-11-28 12:17:16 -0500

    Fix Start Command injection

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,79 +1,72 @@
 #!/bin/bash
 
-# Postgis
-if [[ $* == *--postgis* ]]
-echo "Waiting on postgis"
-then
-  ATTEMPTS=0
-  while true
-  do
-    pg_isready -h postgis
+cmd="$@"
 
-    if [ $? -eq 0 ]
+# Postgis
+ATTEMPTS=0
+while true
+do
+  pg_isready -h postgis
+
+  if [ $? -eq 0 ]
+  then
+    break
+  else 
+    let ATTEMPTS=ATTEMPTS+1
+    if [ $ATTEMPTS -gt 10 ]
     then
-      break
-    else 
-      let ATTEMPTS=ATTEMPTS+1
-      if [ $ATTEMPTS -gt 10 ]
-      then
-        echo "Timed out waiting for postgis"
-        exit 1
-      else
-        sleep 10
-      fi
+      echo "Timed out waiting for postgis"
+      exit 1
+    else
+      sleep 10
     fi
-  done
-  echo "Postgis found"
-fi
+  fi
+done
+echo "Postgis found"
 
 # GeoServer
-if [[ $* == *--geoserver* ]]
-echo "Waiting on geoserver"
-then
-  ATTEMPTS=0
-  while true
-  do
-    curl http://geoserver:8080/geoserver/index.html
+ATTEMPTS=0
+while true
+do
+  curl http://geoserver:8080/geoserver/index.html
 
-    if [ $? -eq 0 ]
+  if [ $? -eq 0 ]
+  then
+    break
+  else 
+    let ATTEMPTS=ATTEMPTS+1
+    if [ $ATTEMPTS -gt 10 ]
     then
-      break
-    else 
-      let ATTEMPTS=ATTEMPTS+1
-      if [ $ATTEMPTS -gt 10 ]
-      then
-        echo "Timed out waiting for geoserver"
-        exit 1
-      else
-        sleep 10
-      fi
+      echo "Timed out waiting for geoserver"
+      exit 1
+    else
+      sleep 10
     fi
-  done
-  echo "Geoserver found"
-fi
+  fi
+done
+echo "Geoserver found"
 
 # RabbitMQ
-if [[ $* == *--rabbitmq* ]]
-echo "Waiting on rabbitmq"
-then
-  ATTEMPTS=0
-  while true
-  do
-    curl rabbitmq:15672
+ATTEMPTS=0
+while true
+do
+  curl rabbitmq:15672
 
-    if [ $? -eq 0 ]
+  if [ $? -eq 0 ]
+  then
+    break
+  else 
+    let ATTEMPTS=ATTEMPTS+1
+    if [ $ATTEMPTS -gt 10 ]
     then
-      break
-    else 
-      let ATTEMPTS=ATTEMPTS+1
-      if [ $ATTEMPTS -gt 10 ]
-      then
-        echo "Timed out waiting for rabbitmq"
-        exit 1
-      else
-        sleep 10
-      fi
+      echo "Timed out waiting for rabbitmq"
+      exit 1
+    else
+      sleep 10
     fi
-  done
-  echo "Rabbitmq found"
-fi
+  fi
+done
+echo "Rabbitmq found"
+
+echo "$cmd"
+$cmd
